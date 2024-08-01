6fb9b5b74b2367b9f1441ff611e3147eb7546195
MertKadakal <mert.kadakal1629@gmail.com>
2024-08-02 02:23:50 +0300

    Oyun mekaniği çoğunlukla oturtuldu

--- TheGame.py
+++ TheGame.py
@@ -13,7 +13,7 @@ def main():
         round = 1
         columns = {"A": 0, "B": 1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8, "J":9}
         ship_names = {"C": "Carrier", "B": "Battleship", "D": "Destroyer", "S": "Submarine", "P": "Patrol Boat"}
-        player1_init_list_hidden = [["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+        player1_init_list_hidden = [["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"], # these lists will be printed on output file
                                     ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
                                     ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
                                     ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
@@ -66,52 +66,67 @@ def main():
             player2_init_list.append(tem_line)
 
         #parts left of battleships and patrol boats
-        patrol_boats_1 = park_gruplarini_belirle(player1_init_list)
-        patrol_boats_2 = park_gruplarini_belirle(player2_init_list)
-        battleships_1 = dortlu_b_gruplarini_belirle(player1_init_list)
-        battleships_2 = dortlu_b_gruplarini_belirle(player2_init_list)
+        patrol_boats_1 = park_gruplarini_belirle(player1_init_list.copy())
+        patrol_boats_2 = park_gruplarini_belirle(player2_init_list.copy())
+        battleships_1 = dortlu_b_gruplarini_belirle(player1_init_list.copy())
+        battleships_2 = dortlu_b_gruplarini_belirle(player2_init_list.copy())
 
         output_file.write("Battle of Ships Game\n\n")
         game_finished = False
 
         while not game_finished:
+
+            #turn for player1
             turn = 1
             output_file.write(f"Player{turn}'s Move\n\nRound: {round}         Grid Size: 10x10\n\n")
             print_hidden_boards(output_file, player1_init_list_hidden, player2_init_list_hidden)
-            if turn == 1:
-                row = int(player1_moves_list[round-1].split(",")[0])-1
-                col = columns[player1_moves_list[round-1].split(",")[1]]
+            row = int(player1_moves_list[round-1].split(",")[0])-1
+            col = columns[player1_moves_list[round-1].split(",")[1]]
 
-                if player2_init_list[row][col] == "O" or player2_init_list[row][col] == "X":
-                    print("This block has been already shot!")
-                elif player2_init_list[row][col] == "-":
-                    player2_init_list_hidden[row][col] = "O"
-                    player2_init_list[row][col] = "O"
-                else:
-                    player2_init_list_hidden[row][col] = "X"
-                    player2_init_list[row][col] = "X"
-                    if check_the_hitten_ship(player2_init_list[row][col], player2_init_list, battleships_2, patrol_boats_2, cbdsp2):
-                        output_file.write(f"Player2's {ship_names[player2_init_list[row][col]]} has been sunk!")
+            if player2_init_list[row][col] == "O" or player2_init_list[row][col] == "X":
+                print("This block has been already shot!")
+            elif player2_init_list[row][col] == "-":
+                player2_init_list_hidden[row][col] = "O"
+                player2_init_list[row][col] = "O"
+            else:
+                ship = player2_init_list[row][col]
+                player2_init_list_hidden[row][col] = "X"
+                player2_init_list[row][col] = "X"
+                if check_the_hitten_ship(ship, player2_init_list, battleships_2, patrol_boats_2, cbdsp2):
+                    print(f"Player2's {ship_names[ship]} has been sunk!")
 
+            #turn for player2
             turn = 2
             output_file.write(f"Player{turn}'s Move\n\nRound: {round}         Grid Size: 10x10\n\n")
             print_hidden_boards(output_file, player1_init_list_hidden, player2_init_list_hidden)
-            if turn == 1:
-                row = int(player1_moves_list[round-1].split(",")[0])
-                col = columns[player1_moves_list[round-1].split(",")[1]]
+            row = int(player2_moves_list[round-1].split(",")[0])-1
+            col = columns[player2_moves_list[round-1].split(",")[1]]
 
-                if player1_init_list[row][col] == "O" or player1_init_list[row][col] == "X":
-                    print("This block has been already shot!")
-                elif player1_init_list[row][col] == "-":
-                    player1_init_list_hidden[row][col] = "O"
-                    player1_init_list[row][col] = "O"
-                else:
-                    player1_init_list_hidden[row][col] = "X"
-                    player1_init_list[row][col] = "X"
-                    if check_the_hitten_ship(player1_init_list[row][col], player1_init_list, battleships_1, patrol_boats_1, cbdsp1):
-                        output_file.write(f"Player1's {ship_names[player1_init_list[row][col]]} has been sunk!")
+            if player1_init_list[row][col] == "O" or player1_init_list[row][col] == "X":
+                print("This block has been already shot!")
+            elif player1_init_list[row][col] == "-":
+                player1_init_list_hidden[row][col] = "O"
+                player1_init_list[row][col] = "O"
+            else:
+                ship = player1_init_list[row][col]
+                player1_init_list_hidden[row][col] = "X"
+                player1_init_list[row][col] = "X"
+                if check_the_hitten_ship(ship, player1_init_list, battleships_1, patrol_boats_1, cbdsp1):
+                    print(f"Player1's {ship_names[ship]} has been sunk!")
+            
+            if cbdsp1 == cbdsp2 == [0,0,0,0,0]:
+                print("Draw!")
+                game_finished = True
+            elif cbdsp1 == [0,0,0,0,0]:
+                print("Player2 won!")
+                game_finished = True
+            elif cbdsp2 == [0,0,0,0,0]:
+                print("Player1 won!")
+                game_finished = True
+
 
             round += 1
+            print(cbdsp1, cbdsp2, round)
 
 
 def print_hidden_boards(output_file, p1, p2):
@@ -143,7 +158,6 @@ def print_hidden_boards(output_file, p1, p2):
     output_file.write("\n")
 
 def park_gruplarini_belirle(grid):
-    init_grid = grid
     satir_sayisi = 10
     sutun_sayisi = 10
     gruplar = []  # Grupları saklamak için liste
@@ -167,11 +181,13 @@ def park_gruplarini_belirle(grid):
                     grid[cift[0][0]][cift[0][1]] = 'Z'
                     grid[cift[1][0]][cift[1][1]] = 'Z'
 
-    grid = init_grid
+    for i in range(4):
+        for j in range(2):
+            grid[gruplar[i][j][0]][gruplar[i][j][1]] = "P"
+
     return gruplar
 
 def dortlu_b_gruplarini_belirle(grid):
-    init_grid = grid
     satir_sayisi = 10
     sutun_sayisi = 10
     gruplar = []  # Grupları saklamak için liste
@@ -194,8 +210,11 @@ def dortlu_b_gruplarini_belirle(grid):
                     # İşaretlenen "BBBB" dörtlülerini "Z" olarak işaretleyerek tekrar kontrol edilmelerini önleyin
                     for (x, y) in dortlu:
                         grid[x][y] = 'Z'
+    
+    for i in range(2):
+        for j in range(4):
+            grid[gruplar[i][j][0]][gruplar[i][j][1]] = "B"
 
-    grid = init_grid
     return gruplar
 
 def check_the_hitten_ship(ship, grid, bat_ship, pat_boats, cbdsp):
@@ -216,21 +235,26 @@ def check_the_hitten_ship(ship, grid, bat_ship, pat_boats, cbdsp):
             return True
         
     if ship == "P":
-        amount = 0
+        pre_amo = cbdsp[4]
+        amount = 4
         for i in range(len(pat_boats)):
             row1 = pat_boats[i][0][0]
             col1 = pat_boats[i][0][1]
             row2 = pat_boats[i][1][0]
             col2 = pat_boats[i][1][1]
             if grid[row1][col1] + grid[row2][col2]  == "XX":
-                amount =+ 1
-        if amount == 4:
+                amount -= 1
+        if amount == 0:
+            cbdsp[4] = amount
             return True
         else:
-            cbdsp[2] = amount
+            cbdsp[4] = amount
+            if amount != pre_amo:
+                return True
         
     if ship == "B":
-        amount = 0
+        pre_amo = cbdsp[1]
+        amount = 2
         for i in range(len(bat_ship)):
             row1 = bat_ship[i][0][0]
             col1 = bat_ship[i][0][1]
@@ -241,11 +265,14 @@ def check_the_hitten_ship(ship, grid, bat_ship, pat_boats, cbdsp):
             row4 = bat_ship[i][3][0]
             col4 = bat_ship[i][3][1]
             if grid[row1][col1] + grid[row2][col2] + grid[row3][col3] + grid[row4][col4]  == "XXXX":
-                amount =+ 1
-        if amount == 2:
+                amount -= 1
+        if amount == 0:
+            cbdsp[1] = amount
             return True
         else:
             cbdsp[1] = amount
+            if amount != pre_amo:
+                return True
 
     return False
 
